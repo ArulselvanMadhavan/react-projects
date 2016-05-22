10e3f7fdff237803b8aca8ce5bca4622874b721c
Arulselvan Madhavan <arulselv@ccs.neu.edu>
2016-05-21 20:59:28 -0400

    Finished the UI

--- voting-client/src/index.jsx
+++ voting-client/src/index.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Voting from './components/Voting';
-
+import {Router,Route,hashHistory} from 'react-router';
+import App from './components/App';
+import Results from './components/Results';
 require('./style.css');
-const pair = ['Trainspotting','28 Days Later'];
+
+const routes = <Route component={App}>
+    <Route path="/" component={Voting}/>
+    <Route path="/results" component={Results} />
+</Route>
 
 ReactDOM.render(
-    <Voting pair={pair} />,
-    document.getElementById('app')
-);
+    <Router history={hashHistory}>{routes}</Router>, document.getElementById('app'));
