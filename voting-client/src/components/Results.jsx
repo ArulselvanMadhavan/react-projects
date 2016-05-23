980894a7e162be4b39fcf95564662baafa0dc906
Arulselvan Madhavan <arulselv@ccs.neu.edu>
2016-05-23 08:48:55 -0400

    Not moving to next

--- voting-client/src/components/Results.jsx
+++ voting-client/src/components/Results.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
 import Winner from './Winner';
+import {connect} from 'react-redux';
+import * as actionCreators from '../action_creators';
 
-export default React.createClass({
+export const Results =  React.createClass({
     mixins: [PureRenderMixin],
     getPair: function() {
         return this.props.pair || [];
@@ -35,3 +37,12 @@ export default React.createClass({
             ));
     }
 });
+
+function mapStateToProps(state){
+    return {
+        pair:state.getIn(['vote','pair']),
+        winner:state.get('winner'),
+        tally:state.getIn(['vote','tally'])
+    }
+}
+export const ResultsContainer = connect(mapStateToProps,actionCreators)(Results);
--- voting-client/src/index.jsx
+++ voting-client/src/index.jsx
@@ -1,15 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Voting from './components/Voting';
+import {VotingContainer} from './components/Voting';
 import {Router,Route,hashHistory} from 'react-router';
 import App from './components/App';
-import Results from './components/Results';
+import {ResultsContainer} from './components/Results';
+import {createStore, applyMiddleware} from 'redux';
+import reducer from './reducer';
+import {Provider} from 'react-redux';
+import io from 'socket.io-client';
 require('./style.css');
+import {setState} from './action_creators.js';
+import remoteActionMiddleware from './remote_action_middleware';
+
+const socket = io(`${location.protocol}//${location.hostname}:8090`);
+socket.on('state', (state) =>
+  store.dispatch(setState(state))
+);
+
+const createStoreWithMiddleware = applyMiddleware(
+  remoteActionMiddleware(socket)
+)(createStore);
+const store = createStoreWithMiddleware(reducer);
+
+store.dispatch({
+    type:'SET_STATE',
+    state:{
+        vote:{
+            pair:['Sunshine','28 Days Later'],
+            tally:{Sunshine:2}
+        }
+    }
+});
 
 const routes = <Route component={App}>
-    <Route path="/" component={Voting}/>
-    <Route path="/results" component={Results} />
+    <Route path="/" component={VotingContainer}/>
+    <Route path="/results" component={ResultsContainer} />
 </Route>
 
 ReactDOM.render(
-    <Router history={hashHistory}>{routes}</Router>, document.getElementById('app'));
+    <Provider store={store}>
+        <Router history={hashHistory}>{routes}</Router>
+    </Provider>,
+    document.getElementById('app'));
